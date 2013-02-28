dc9795c1fd85b8103b19296bf24a60f3d195ea30
Dimitar Dimitrov <me@ddimitrov.name>
2013-02-28 20:21:42 +0200

    Fix $.event.trigger for jQuery 1.9+
    jQuery 1.9+ removes $.event.trigger so we should use jQuery.fn.trigger() instead.

--- jquery.infinitescroll.js
+++ jquery.infinitescroll.js
@@ -755,7 +755,7 @@
 
             if (scrollTimeout) { clearTimeout(scrollTimeout); }
             scrollTimeout = setTimeout(function () {
-                $.event.handle.apply(context, args);
+                $(context).trigger('smartscroll', args);
             }, execAsap === "execAsap" ? 0 : 100);
         }
     };
